00b7549523474ccb408fa7a6c7ebca59769ed038
jlttt <jlttt@laposte.net>
2019-01-31 12:09:10 +0100

    step-13

--- config/config.php
+++ config/config.php
@@ -5,6 +5,8 @@ use Http\Adapter\Guzzle6\Client;
 use Http\Client\HttpClient;
 
 use Ubi\Deploy\Deploy;
+use Ubi\Deploy\Service\SlugFilter;
+use Ubi\Deploy\Service\VersionFilter;
 
 $definition = new Definition();
 $definition->setAutowired(true);
@@ -13,3 +15,10 @@ $loader->registerClasses($definition, 'Ubi\\Deploy\\', __DIR__ . '/../src/');
 $container->register(HttpClient::class, Client::class);
 $container->getDefinition(Deploy::class)
     ->setPublic(true);
+$container->getDefinition(SlugFilter::class)
+    ->addArgument('%allowed_slugs%')
+    ->setPublic(true);
+$container->getDefinition(VersionFilter::class)
+    ->addArgument('%allowed_versions%')
+    ->setPublic(true);
+
---
+++ config/parameters.php
@@ -0,0 +1,3 @@
+<?php
+$container->setParameter('allowed_slugs', ['dev', 'preprod']);
+$container->setParameter('allowed_versions', ['2.62', '2.61']);
