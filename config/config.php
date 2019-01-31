e56f315b2584a4f0fa922c5d1480d6de88dbcc2d
jlttt <jlttt@laposte.net>
2019-01-31 14:52:57 +0100

    step-15

--- config/config.php
+++ config/config.php
@@ -8,21 +8,25 @@ use Http\Client\HttpClient;
 use Ubi\Deploy\Deploy;
 use Ubi\Deploy\Service\SlugFilter;
 use Ubi\Deploy\Service\VersionFilter;
+use Ubi\Deploy\Service\Filter;
 
 $definition = new Definition();
 $definition->setAutowired(true);
+$definition->setAutoconfigured(true);
+$definition->setPublic(false);
 
 $loader->registerClasses($definition, 'Ubi\\Deploy\\', __DIR__ . '/../src/');
+$container->registerForAutoconfiguration(Filter::class)->addTag('ubi.deploy.filter');
+
 $container->register(HttpClient::class, Client::class);
+
 $container->getDefinition(SlugFilter::class)
-    ->addArgument('%allowed_slugs%')
-    ->setPublic(true);
+    ->addArgument('%allowed_slugs%');
+
 $container->getDefinition(VersionFilter::class)
-    ->addArgument('%allowed_versions%')
-    ->setPublic(true);
+    ->addArgument('%allowed_versions%');
+
 $container->getDefinition(Deploy::class)
-    ->addMethodCall('addFilter', [new Reference(VersionFilter::class)])
-    ->addMethodCall('addFilter', [new Reference(SlugFilter::class)])
     ->setPublic(true);
 
 
