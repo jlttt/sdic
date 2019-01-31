3cf30a3e5f2d7b10a6cc63599a0da3c37cf9842a
jlttt <jlttt@laposte.net>
2019-01-31 12:18:56 +0100

    step-14

--- config/config.php
+++ config/config.php
@@ -1,5 +1,6 @@
 <?php
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 
 use Http\Adapter\Guzzle6\Client;
 use Http\Client\HttpClient;
@@ -13,12 +14,15 @@ $definition->setAutowired(true);
 
 $loader->registerClasses($definition, 'Ubi\\Deploy\\', __DIR__ . '/../src/');
 $container->register(HttpClient::class, Client::class);
-$container->getDefinition(Deploy::class)
-    ->setPublic(true);
 $container->getDefinition(SlugFilter::class)
     ->addArgument('%allowed_slugs%')
     ->setPublic(true);
 $container->getDefinition(VersionFilter::class)
     ->addArgument('%allowed_versions%')
     ->setPublic(true);
+$container->getDefinition(Deploy::class)
+    ->addMethodCall('addFilter', [new Reference(VersionFilter::class)])
+    ->addMethodCall('addFilter', [new Reference(SlugFilter::class)])
+    ->setPublic(true);
+
 
